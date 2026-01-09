3978ffc8e109fe5ca7a22c87b5f5877d4d949ddc
Rajvardhan Yadav <rajvardhanyadav0602@gmail.com>
2026-01-09 13:56:04 +0530

    Add files via upload

---
+++ Assignment_No_7/04_Array_of_7_Elements_with_Null_Initialization_Accept_All_Elements_without_Loop_Display_Its.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+
+{
+    int Arr[50] = {};
+
+    printf("\n\n Value of Element :");
+    scanf("%d",&Arr[0]);
+    printf("\n\n Value of Element :");
+    scanf("%d",&Arr[1]);
+    printf("\n\n Value of Element :");
+    scanf("%d",&Arr[2]);
+    printf("\n\n Value of Element :");
+    scanf("%d",&Arr[3]);
+    printf("\n\n Value of Element :");
+    scanf("%d",&Arr[4]);
+    printf("\n\n Value of Element :");
+    scanf("%d",&Arr[5]);
+    printf("\n\n Value of Element :");
+    scanf("%d",&Arr[6]);
+
+    getch();
+
+    printf("\n Number is :  %d",Arr[0]);
+    printf("\n Number is :  %d",Arr[1]);
+    printf("\n Number is :  %d",Arr[2]);
+    printf("\n Number is :  %d",Arr[3]);
+    printf("\n Number is :  %d",Arr[4]);
+    printf("\n Number is :  %d",Arr[5]);
+    printf("\n Number is :  %d",Arr[6]);
+
+    getch();
+    return 0;
+}
