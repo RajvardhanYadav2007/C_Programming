2ef7a7102df7374cae0654af5e3fb44ed8e1e6eb
Rajvardhan Yadav-Patil <rajvardhanyadav0602@gmail.com>
2026-05-30 23:05:08 +0530

    Add files via upload

---
+++ Assignment_No_8/01_Function_to_Print_a_Welcome_Message.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+void Welcome();
+
+int main()
+{
+    Welcome();
+
+    getch();
+    return 0;
+}
+void Welcome()
+{
+    printf("Welcome To C Programming");
+}
+
+
+
+
+
