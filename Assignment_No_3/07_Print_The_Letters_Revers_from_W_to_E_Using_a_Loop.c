322b1261b778a6e48eed518731f23513d1a754ca
Rajvardhan Yadav-Patil <rajvardhanyadav0602@gmail.com>
2026-06-17 22:55:25 +0530

    Add files via upload

---
+++ Assignment_No_3/07_Print_The_Letters_Revers_from_W_to_E_Using_a_Loop.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char ch = '0';
+
+    ch = 'W';
+
+    while ( W <= 'E')
+    {
+       printf("\n %c ",ch);
+       ch++;
+    }
+    getch();
+    return 0;
+}
