e3591c5a4d0624b1b2f71a1671731c32d3f4b77e
Rajvardhan Yadav-Patil <rajvardhanyadav0602@gmail.com>
2026-07-04 21:57:07 +0530

    Add files via upload

---
+++ Assignment_No_4/2A_Print_Square_Star_Pattern.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int i = 0, j = 0;
+
+  printf("\n ============******=========\n");
+
+  for(i = 1; i <= 4; i++)
+  {
+    for(j = 1; j <= 4; j++)
+    {
+      printf(" * ");
+    }
+    printf("\n");
+  }
+  printf("\n ============******=========\n");
+
+  getch();
+  return 0;
+}
+
