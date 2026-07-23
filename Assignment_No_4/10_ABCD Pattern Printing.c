5fe8b6be10c2202821240150cac5183a35dfdec8
Rajvardhan Yadav-Patil <rajvardhanyadav0602@gmail.com>
2026-07-23 22:24:21 +0530

    Add files via upload

---
+++ Assignment_No_4/10_ABCD Pattern Printing.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char ch ='/0';
+
+    int i = 0,j = 0,RC = 0;
+
+    printf("\n Enter A Row And Column:");
+    scanf("%d",&RC);
+
+    for(i= 1 ,ch ='A';i <= RC;i++)
+    {
+        for(j = 1;j <= RC;j++)
+        {
+            printf(" %c ",ch);
+            ch++;
+        }
+            printf("\n");
+    }
+        getch();
+        return 0;
+}
