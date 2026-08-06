cbdd70adafb8f7f259a1fc6270c2d78aa2cf9e88
RAJVARDHAN YADAV-PATIL <rajvardhanyadav0602@gmail.com>
2026-08-06 23:04:01 +0530

    Add files via upload

---
+++ Assignment_No_4/13_Hollow Dimond.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int n , i ,j;
+
+    pintf("\n Enter a Number Row : ");
+    scanf("%d",&n);
+
+    printf("\n Pattern is : \n\n");
+
+    for(i = 1; i<= n; i++)
+    {
+        for(j = 1; j <= n-i; j++)
+        {
+            printf("   ");
+        }
+        for(j = 1 ; j<= 2*i-1; j++)
+        {
+            if( j == 1 || j == 2 * i -1)
+
+                printf(" * ");
+            else
+            printf("   ");
+        }
+        printf("\n");
+    }
+    for(i = n -1; i>=1; i--)
+    {
+        for( j =1; j<= n- i; j++)
+        {
+            printf("   ");
+        }
+        for(j = 1; j <= 2*i-1; j++)
+        {
+            if(j == 1 || j == 2*i-1)
+            printf(" * ");
+            else
+            printf("   ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
